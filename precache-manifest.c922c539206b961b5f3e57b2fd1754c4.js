90159782877d2a3ca0d62be71bc782952e921e73
Harshil Pate <hrpatel.263@gmail.com>
2019-01-27 10:18:23 -0800

    Updates

---
+++ precache-manifest.c922c539206b961b5f3e57b2fd1754c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/proprtysite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "408e5a565e768335c7c37094dd8bc050",
+    "url": "/proprtysite/static/media/background-img.408e5a56.jpg"
+  },
+  {
+    "revision": "eda492b8a9ddfdca28de",
+    "url": "/proprtysite/static/js/runtime~main.eda492b8.js"
+  },
+  {
+    "revision": "7c1a20b92c0846301db5",
+    "url": "/proprtysite/static/js/main.7c1a20b9.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/proprtysite/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "7c1a20b92c0846301db5",
+    "url": "/proprtysite/static/css/main.55bbc074.chunk.css"
+  },
+  {
+    "revision": "1ea030fa943faf0019f4e8e32a476257",
+    "url": "/proprtysite/index.html"
+  }
+];
